d6d89fc586fa2d4d189dd9c175a91409ecf91931
Me <Zizo@AhmedGhalab.local>
2013-04-11 23:33:16 +0200

    Adding Empty Files of Project to Repo

---
+++ babycare/babycare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  babycare
+//
+//  Created by Me on 4/11/13.
+//  Copyright (c) 2013 Toledo, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ babycare/babycare/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  babycare
+//
+//  Created by Me on 4/11/13.
+//  Copyright (c) 2013 Toledo, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
